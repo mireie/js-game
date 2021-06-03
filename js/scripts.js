493b01278c276f56527afef724ca2e11874c4ffb
mireie <michael.reiersgaard@gmail.com>
2021-06-03 14:32:51 -0700

    git init commit

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+/* create a bar that takes up 100% of the screen.
+roll a number
+put that number in a variable (let), add that variable to the width of the color portion of the loading bar.
+maintain variable, add or subtract that number from a made variable and "reset" width dimensions in thto match
+*/
+
+
