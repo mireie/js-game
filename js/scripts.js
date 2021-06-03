9b51b30b512c062c64f35cc9ed711d839648f85c
mireie <michael.reiersgaard@gmail.com>
2021-06-03 16:39:59 -0700

    pair programming, this is a bad commit Co-authored-by: Kyle Kay-Perez <Kyle.kay-perez@gmail.com> Co-authored-by: Kristen Hopper <HopperDavis@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,51 @@
-/* create a bar that takes up 100% of the screen.
-roll a number
-put that number in a variable (let), add that variable to the width of the color portion of the loading bar.
-maintain variable, add or subtract that number from a made variable and "reset" width dimensions in thto match
-*/
+function addWidth(currentWidth, progress) {
+  if ((currentWidth + progress)>100) {
+    return currentWidth - (2 * progress);
+  }
+  else {
+    return currentWidth + progress;
+  }
+}
 
+function addWidth2(currentWidth2, progress2) {
+  if ((currentWidth2 + progress2)>100) {
+    return currentWidth2 - (2 * progress2);
+  }
+  else {
+    return currentWidth2 + progress2;
+  }
+}
 
+let currentWidth = 0;
+let currentWidth2 = 0;
+
+$(document).ready(function() {
+  $("img#dice").click(function() {
+    let progress = Math.floor(Math.random() * 20) + 1;
+    let result = addWidth(currentWidth, progress);
+    currentWidth = result;
+    console.log(progress+", "+currentWidth);
+    $("#myBar").width(result + "%");
+    $("#coolplayer").text(progress);
+    
+
+    let progress2 = Math.floor(Math.random() * 20) + 1;
+    let result2 = addWidth2(currentWidth2, progress2)
+    currentWidth2 = result2
+    console.log(progress2+", "+currentWidth);
+    $("#enemyBar").width(result2 + "%");
+    $("#enemyplayer").text(progress2);
+    
+    if (currentWidth === 100 || currentWidth2 === 100) {
+      $(".hidden").show();
+    }
+  });
+
+  $("button#reset").click(function() {
+    $(".hidden").hide();
+    let currentWidth = 0;
+    let currentWidth2 = 0;
+    $("#myBar").width(currentWidth + "%");
+    $("#enemyBar").width(currentWidth2 + "%");
+  });
+});
